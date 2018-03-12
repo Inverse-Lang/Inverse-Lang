3f01636104e736baa700f1223d76adb07314e69c
dmelcer9 <dmelcer9@gmail.com>
2018-03-12 16:42:53 -0400

    Create and apply noninvertible lambda functions

--- lib/lang.rkt
+++ lib/lang.rkt
@@ -1,2 +1,33 @@
 #lang racket
 
+(require "function-structs.rkt")
+(require (prefix-in un: racket))
+(require (for-syntax syntax/parse))
+
+(provide #%module-begin)
+
+; Define a constant
+; Provides a shorthand to define a non-invertible function
+; which expands to a lambda
+(define-syntax define-func-ninv
+  (syntax-parser
+    [(_ (name args ...) body)
+     #'(un:define name (lambda-ninv (args ...) body))]
+    [(_ name body)
+     #'(un:define name body)]))
+
+; Create a non-invertible lambda
+(define-syntax lambda-ninv
+  (syntax-parser
+    [(_ (args ...) body)
+     #'(ninvfunc-wrap (un:lambda (args ...) body))]))
+
+; Applies a function, taking it out of its wrapper struct
+(define-syntax apply-func
+  (syntax-parser
+    [(_ func args ...)
+     #'(begin (define realfunc
+                (cond
+                  [(invfunc-wrap? funcy) (invfunc-wrap-func func)]
+                  [(ninvfunc-wrap? func) (ninvfunc-wrap-func func)]))
+              (un:#%app realfunc args ...))])) 
