2707d55f470d0eff8affe799a44a823cd80931f0
dmelcer9 <dmelcer9@gmail.com>
2018-03-29 21:05:24 -0400

    Work on docs a little

--- doc/manual/TIL.rkt
+++ doc/manual/TIL.rkt
@@ -21,6 +21,23 @@ The inverse language allows a user to construct and use the algebraic inverses o
  or an error if the provided function is not invertible.
 }
 
+@deftogether[(@defform[(lambda-create-invertible (argument) body invbody)]
+               @defform[(λ-create-invertible (argument) body invbody)])]{
+ Creates an invertible function with a specified body and inverse.
+}
+
+@deftogether[(@defform[(lambda-auto-invert (argument) body)]
+               @defform[(λ-auto-invert (argument) body)])]{
+ Automatically creates the inverse of a function provided that
+ the body of the function meets specific constraints.
+
+ @itemize{
+  @item{
+   
+  }
+ }
+}
+
 @section{Arithmetic}
 
 @section{Strings}
