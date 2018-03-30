a1f97c3b0ec804cbffe211ed8fbaf176c1a69a7a
dmelcer9 <dmelcer9@gmail.com>
2018-03-29 20:31:47 -0400

    Start docs

--- doc/manual/TIL.rkt
+++ doc/manual/TIL.rkt
@@ -7,13 +7,19 @@
 The inverse language allows a user to construct and use the algebraic inverses of functions.
 
 @racketblock[
-> (add1 5)
-6
-> ((invert add1) 5)
-4
-]
+ > (add1 5)
+ 6
+ > ((invert add1) 5)
+ 4
+ ]
 
 @section{Core Language Features}
+
+@defproc[(invert [func invertible?])
+         invertible?]{
+ Returns the inverse of an invertible function,
+ or an error if the provided function is not invertible.
+}
 
 @section{Arithmetic}
 
